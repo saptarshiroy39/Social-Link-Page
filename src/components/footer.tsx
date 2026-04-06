0431f56d620fbd93413471592e6d8e3d0bb026ce
Saptarshi Roy <saptarshiroy39@gmail.com>
2026-04-06 11:08:47 +0530

    Remove All Sounds

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -6,15 +6,6 @@ import { DATA } from "@/data/resume";
 export default function Footer() {
   const currentYear = new Date().getFullYear();
 
-  function handleClick(url: string) {
-    window.open(url, "_blank", "noopener,noreferrer");
-  }
-
-  function playSound(src: string) {
-    const audio = new Audio(src);
-    audio.play().catch((err) => console.error("Error playing sound:", err));
-  }
-
   return (
     <footer id="footer" className="w-full -mt-10" aria-label="Footer">
       <p className="font-mono text-muted-foreground text-center">
@@ -22,7 +13,7 @@ export default function Footer() {
         <Button
           variant="link"
           className="p-0 hover:cursor-pointer"
-          onClick={() => playSound("/meow.mp3")}
+          onClick={() => window.scrollTo({ top: 0, behavior: "smooth" })}
         >
           <u>{DATA.initials}</u>
         </Button>
@@ -30,7 +21,11 @@ export default function Footer() {
         <Button
           variant="link"
           className="px-2 py-0 hover:cursor-pointer min-w-[20px]"
-          onClick={() => playSound("/faahhh.mp3")}
+          onClick={() => {
+            document
+              .getElementById("projects")
+              ?.scrollIntoView({ behavior: "smooth" });
+          }}
         >
           <u>.</u>
         </Button>
